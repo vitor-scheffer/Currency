1c1e88aa56b27309c9aeeb882161fb5138deb119
Vitor Boff <vitor.boff@DBC-001824.bbrouter>
2023-04-02 18:30:45 -0300

    Add method onClick on btnConverter.

--- app/src/main/java/com/vitorscheffer/currency/MainActivity.kt
+++ app/src/main/java/com/vitorscheffer/currency/MainActivity.kt
@@ -2,10 +2,25 @@ package com.vitorscheffer.currency
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var result: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnConverter = findViewById<Button>(R.id.btn_converter)
+
+        result = findViewById<TextView>(R.id.txt_result)
+
+        btnConverter.setOnClickListener {
+            result.visibility = View.VISIBLE
+            result.text = "Novo Texto"
+        }
+
     }
 }
